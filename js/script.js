befe051a2455c4380b59c24de82f2fb9ef5dab72
GalitskyKK <galitskikhn@mail.ru>
2023-08-16 00:33:17 +0500

    wip

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// const menuToggle = document.querySelector('.menu-toggle');
-// const menuItems = document.querySelector('.menu-items');
-
-// menuToggle.addEventListener('click', () => {
-//    menuItems.classList.toggle('show');
-// });
-
 const menuToggle = document.querySelector('.menu-toggle');
 
 menuToggle.addEventListener('click', function () {
@@ -15,3 +8,56 @@ menuToggle.addEventListener('click', function () {
       menuItems.style.display = 'block';
    }
 });
+
+const btnUp = {
+   el: document.querySelector('.button-up'),
+   show() {
+      if (this.el.classList.contains('button-up_hide') && !this.el.classList.contains('button-up_hiding')) {
+         this.el.classList.remove('button-up_hide');
+         this.el.classList.add('button-up_hiding');
+         window.setTimeout(() => {
+            this.el.classList.remove('button-up_hiding');
+         }, 300);
+      }
+   },
+   hide() {
+      if (!this.el.classList.contains('button-up_hide') && !this.el.classList.contains('button-up_hiding')) {
+         this.el.classList.add('button-up_hiding');
+         window.setTimeout(() => {
+            this.el.classList.add('button-up_hide');
+            this.el.classList.remove('button-up_hiding');
+         }, 300);
+      }
+   },
+   addEventListener() {
+      // при прокрутке окна (window)
+      window.addEventListener('scroll', () => {
+         const scrollY = window.scrollY || document.documentElement.scrollTop;
+         if (this.scrolling && scrollY > 0) {
+            return;
+         }
+         this.scrolling = false;
+         // если пользователь прокрутил страницу более чем на 200px
+         if (scrollY > 400) {
+            // сделаем кнопку .btn-up видимой
+            this.show();
+         } else {
+            // иначе скроем кнопку .btn-up
+            this.hide();
+         }
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.button-up').onclick = () => {
+         this.scrolling = true;
+         this.hide();
+         // переместиться в верхнюю часть страницы
+         window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+         });
+      }
+   }
+}
+
+btnUp.addEventListener();
